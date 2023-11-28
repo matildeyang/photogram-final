2de09ba8b16aef7dfeb5b8cdc0ce8ba8294d6cec
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-28 22:49:37 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_username", { :controller => "users", :action => "show"})
+
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show"})
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "delete"})
+
+  get("/insert_photo", { :controller => "photos", :action => "insert"})
+  get("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
+
+  get("/add_comment", { :controller => "photos", :action => "comment"})
+
 end
