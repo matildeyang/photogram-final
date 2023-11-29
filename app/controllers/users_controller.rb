5780372421e7db7735c6f2b73c48bf0159adcdc3
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-11-29 04:55:22 +0000

    ordering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
 
-    @list_of_users = matching_users.order({ :created_at => :desc })
+    @list_of_users = matching_users.order({ :username => :asc })
 
     render({ :template => "users/index" })
   end
